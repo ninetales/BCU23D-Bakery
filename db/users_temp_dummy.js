8ff9d402c0adcf342f863d82ab3cf991a9e4fa29
Oskar Assarsson <oskar@assarsson.nu>
2023-12-14 20:17:15 +0100

    orders.js added, user_temp_dummy.js added with data

---
+++ db/users_temp_dummy.js
@@ -0,0 +1,55 @@
+const users = [
+    { 
+        id: 1, 
+        username: "admin", 
+        role: "admin" 
+    },
+    { 
+        id: 2, 
+        username: "user1",
+        password: "123",
+        role: "user",
+        logged: false,
+        order_ids: []
+    },
+    { 
+        id: 3, 
+        username: "user2", 
+        password: "123",
+        role: "user",
+        logged: false,
+        order_ids: []
+    },
+    { 
+        id: 4, 
+        username: "user3", 
+        password: "123",
+        role: "user",
+        logged: false,
+        order_ids: []
+    },
+    { 
+        id: 5, 
+        username: "user4", 
+        password: "123",
+        role: "user",
+        logged: false,
+        order_ids: [] 
+    },
+    { 
+        id: 6, 
+        username: "user5", 
+        password: "123",
+        role: "user",
+        logged: false,
+        order_ids: []
+    },
+    { 
+        id: 7, 
+        username: "user6", 
+        password: "123",
+        role: "user",
+        logged: false,
+        order_ids: []
+    }
+  ];
