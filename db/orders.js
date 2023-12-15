7ed31317a2fecc90075af8851c841f2822b927aa
Oskar Assarsson <oskar@assarsson.nu>
2023-12-15 13:40:32 +0100

    reset branch, only order.js db and user_temp.js left

--- db/orders.js
+++ db/orders.js
@@ -1,10 +1,104 @@
 const orders = [
     {
-        id: 1,                      // ID känns rätt bra, som ref till andra dummy data...
-        user_id: 3,                 // ...som här där man får tag på info från users db. Hur omfattande infp?
-        product_ids: [3, 4, 5],     // För att skriva ut sammanfattning av produkter
-        order_date: '14-12-2023',   // När lades ordern
-        process_date: null,         // Datum när ordern är behandlad och skickad. Lägga till en för lev datum?
-        price_total: 1239           // Ifylld manuellt, men räknas ihop när ordern skapas. Både In och ut ?? 
-    }
+        id: 1,
+        user_id: 3,
+        order_products: [
+            {
+                product_id: 1,
+                amount_pkg: 2,
+                price: 14
+            },
+            {
+                product_id: 2,
+                amount_pkg: 4,
+                price: 28
+            },
+            {
+                product_id: 20,
+                amount_pkg: 2,
+                price: 10
+            },
+
+        ],
+        order_date: '9-12-2023',
+        process_date: '10-12-2023',
+        lev_date: '12-12-2023',
+        price_total: 1239
+    },
+    {
+        id: 2,
+        user_id: 2,
+        order_products: [
+            {
+                product_id: 3,
+                amount_pkg: 2,
+                price: 14
+            },
+            {
+                product_id: 4,
+                amount_pkg: 4,
+                price: 28
+            },
+            {
+                product_id: 17,
+                amount_pkg: 2,
+                price: 10
+            },
+        ],
+        order_date: '12-12-2023',
+        process_date: null,
+        lev_date: null,
+        price_total: 1239
+    },
+    {
+        id: 6,
+        user_id: 3,
+        order_products: [
+            {
+                product_id: 9,
+                amount_pkg: 2,
+                price: 14
+            },
+            {
+                product_id: 13,
+                amount_pkg: 4,
+                price: 28
+            },
+            {
+                product_id: 18,
+                amount_pkg: 2,
+                price: 10
+            },
+
+        ],
+        order_date: '9-10-2023',
+        process_date: '10-10-2023',
+        lev_date: '12-10-2023',
+        price_total: 1239
+    },
+    {
+        id: 5,
+        user_id: 2,
+        order_products: [
+            {
+                product_id: 7,
+                amount_pkg: 2,
+                price: 14
+            },
+            {
+                product_id: 8,
+                amount_pkg: 4,
+                price: 28
+            },
+            {
+                product_id: 14,
+                amount_pkg: 2,
+                price: 10
+            },
+        ],
+        order_date: '12-12-2023',
+        process_date: null,
+        lev_date: null,
+        price_total: 1239
+    },
 ]
