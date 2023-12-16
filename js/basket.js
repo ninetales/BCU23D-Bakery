200f60de5002ffb4917d293d29303bff08808130
Mikael Sundh <mikael.sundh@medieinstitutet.se>
2023-12-16 06:03:55 +0100

    basket: added populateBasketList function, added function testing console.logs

--- js/basket.js
+++ js/basket.js
@@ -3,6 +3,7 @@
 
 // =========  GLOBAL Variables  =========
 let basket = [];
+let basketList;
 
 function addToBasket(productId, amount) {
   let productIndex = basket.findIndex((item) => item.productId === productId);
@@ -13,6 +14,8 @@ function addToBasket(productId, amount) {
   }
   //save the basket to local storage to allow it to be read on other pages 
   localStorage.setItem('basket', JSON.stringify(basket));
+
+  populateBasketList();
 } // =========  END GLOBAL Variables  =========
 
 // =========  updateBasketCount()  =========
@@ -25,14 +28,62 @@ function updateBasketCount() { //updates the basket icon to show the number of i
   basketCountElement.textContent = totalCount; //updates basketCountElement with totalCount value
 } // =========  END updateBasketCount()  =========
 
+// ========= Populate teh basket list with products from items added to the basket array  =========
+function populateBasketList() {
+  console.log("basket", basket);
+  console.log("populateBasketList called");
+  if (!basketList) return; //Ensure the basketList is available
+
+  basketList.innerHTML = ""; //Clear the current contents of the basket list
+
+  basket.forEach(basketItem => { //Iterate through each item in the basket array
+    const product = products.find(p => p.id === basketItem.productId); //Find the product in the products array that matches the productId in the basket array
+  
+    console.log('product', product);
+
+    if (product) {
+      let listItem = document.createElement('li');
+      listItem.textContent = `${product.name} - Quantity: ${basketItem.amount}`;
+
+        console.log('basketItem2', basketItem);
+      
+      const priceText = typeof product.price === 'number'
+        ? `$${product.price.toFixed(2)}`
+        : 'Price not available';
+      listItem.textContent += ` - ${priceText}`;
+
+      if(product.image && product.image.length > 0) {
+        let img = document.createElement('img');
+        img.setAttribute('src', product.image[0].src);
+        img.setAttribute('alt', product.name);
+        listItem.appendChild(img);
+      }
+
+        console.log('basketItem3', basketItem);
+
+      basketList.appendChild(listItem);
+    }
+  });
+}
+// =========  END Populate teh basket list with products from items added to the basket array  =========
+
 
 // =========  DOM functionality  =========
-document.addEventListener("DOMContentLoaded", function () {  
+document.addEventListener("DOMContentLoaded", function () { 
+  //Retrieve the UL element.
+  basketList = document.querySelector('.basket__items');
+
   const savedBasket = localStorage.getItem("basket"); //Keeps the basket count up to date when the page is refreshed
   if (savedBasket) {
     basket = JSON.parse(savedBasket);
   }
   updateBasketCount();
+  populateBasketList();
+
+  console.log('dom_basket', basketList);
+  console.log('dom_basket', basket);
+  console.log('products', products);
+  console.log('localStorage', localStorage);
 }); // =========  END DOM functionality  =========
 
 
@@ -49,9 +100,8 @@ document.addEventListener('DOMContentLoaded', function () {
   }
 });
 
-//Retrieve the UL element.
-let basketList = document.querySelector('.basket__items');
 
+/* ===========  Testing new version ==================
 for (let i = 0; i < 3 && i < products.length; i++) {
   let product = products[i];
   let listItem = document.createElement('li');
@@ -70,7 +120,7 @@ for (let i = 0; i < 3 && i < products.length; i++) {
 
   basketList.appendChild(listItem);
   console.log('Image source:', product.image[0].src);
-}
+}// ==========  Testing new version ==============*/
 
 //makes sure an update on any tab is reflected on all tabs
 window.addEventListener("storage", function (basketMemory) {
