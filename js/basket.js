48c42fce36f93d24788fe1ddbed6c9438609665f
Mikael Sundh <mikael.sundh@medieinstitutet.se>
2023-12-16 04:13:30 +0100

    basket: combined basket.js and basket-pop.js into one file, removed script links in *.html files

--- js/basket.js
+++ js/basket.js
@@ -1,3 +1,43 @@
+//creates the array to save the productId and the amount for each product to be read into the basket
+//potentially this coule be lifted into the basket.js file, but I am not sure where it would go so to not mess up Jonathans code
+
+// =========  GLOBAL Variables  =========
+let basket = [];
+
+function addToBasket(productId, amount) {
+  let productIndex = basket.findIndex((item) => item.productId === productId);
+  if (productIndex !== -1) {
+    basket[productIndex].amount += amount;
+  } else {
+    basket.push({ productId, amount });
+  }
+  //save the basket to local storage to allow it to be read on other pages 
+  localStorage.setItem('basket', JSON.stringify(basket));
+} // =========  END GLOBAL Variables  =========
+
+// =========  updateBasketCount()  =========
+function updateBasketCount() { //updates the basket icon to show the number of items in the basket
+  const basketCountElement = document.querySelector(
+    ".header-button__basket-count"
+  );
+  if (!basketCountElement) return; // If basket button doesn't exist, stop function
+  let totalCount = basket.reduce((sum, item) => sum + item.amount, 0); //0 signifies the initial value of the sum 
+  basketCountElement.textContent = totalCount; //updates basketCountElement with totalCount value
+} // =========  END updateBasketCount()  =========
+
+
+// =========  DOM functionality  =========
+document.addEventListener("DOMContentLoaded", function () {  
+  const savedBasket = localStorage.getItem("basket"); //Keeps the basket count up to date when the page is refreshed
+  if (savedBasket) {
+    basket = JSON.parse(savedBasket);
+  }
+  updateBasketCount();
+}); // =========  END DOM functionality  =========
+
+
+
+
 // Makes basket button go to basket.html
 document.addEventListener('DOMContentLoaded', function () {
   const basketButton = document.querySelector('.header-button__basket');
@@ -31,5 +71,12 @@ for (let i = 0; i < 3 && i < products.length; i++) {
   basketList.appendChild(listItem);
   console.log('Image source:', product.image[0].src);
 }
+
+//makes sure an update on any tab is reflected on all tabs
+window.addEventListener("storage", function (basketMemory) {
+  if (basketMemory.key === "basket") {
+    basket = JSON.parse(basketMemory.newValue);
+  }
+});
 
 console.log(products);
